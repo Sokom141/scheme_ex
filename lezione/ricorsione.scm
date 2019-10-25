2ff11a5f7b9d2408e6333e655723ccfefcdcb6f0
sokom141 <sokom141@gmail.com>
2019-10-25 18:57:34 +0200

    commenti ricorsione

--- lezione/ricorsione.scm
+++ lezione/ricorsione.scm
@@ -3,6 +3,7 @@
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname ricorsione) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ; Ricorsione -> possibile risolvere qualsiasi problema
 
+; controlla che la stringa passata sia uguale a "0" e in questo caso restituisce "1", in tutti gli altri "0"
 (define cmp   ; val: stringa
   (lambda (b) ; b: stringa "0" oppure "1"
     (if (string=? b "0")
@@ -12,31 +13,34 @@
     )
   )
 
+; Funzione ricorsiva, divide la stringa fin quando non gestisce stringhe di un solo carattere con la procedura cmp
 (define compl-1 ; val: stringa
   (lambda (s)   ; s: stringa di 0/1
-    (let ((k (quotient (string-length s)))
-      )
-#|    (if (> (string-length s) 0)
+    (let ( (k (quotient (string-length s) 2)) )
+      (if (> k 0)
+          (string-append
+           (compl-1 (substring s 0 k))
+           (compl-1 (substring s k))
+           )
+          (cmp s)
+          )
+      #|Metodo senza l'utlizzo di let:
+    (if (> (string-length s) 0)
         (string-append
          (cmp (substring s 0 1))
          (compl-1 (substring s 1))
          )
         ""
         ) |#
-    (if (> k 0)
-        (string-append
-         (compl-1 (substring s 0 k))
-         (compl-1 (substring s k))
-         )
-        (cmp s)
-        )
+      )
     )
-  ))
+  )
 
-(define bin-rep ; dal numero decimale restituisce binario
-  (lambda (n)
+; dal numero decimale restituisce binario
+(define bin-rep   ; val:
+  (lambda (n)     ;   n:
     (let ( (q (quotient n 2)) (r (remainder n 2)) )
-      (let ( (lsb (if (= r 0) "0" "1")) )
+      (let ( (lsb (if (= r 0) "0" "1")) )   ; lsb: last significant bit
         (if (= q 0)
             lsb
             (string-append (bin-rep q) lsb)
@@ -63,8 +67,9 @@ compito:
 
 
 
-; (let ((k1 E1) (k2 E2) ..)
+; (let ((k1 E1) (k2 E2) ..) )
 
+; Data una stringa non vuota di 0/1 restituisce una stringa contenente la conversione in decimale del numero
 (define bin-val   ; val: intero
   (lambda (num)   ; num: stringa non vuota di 0/1
     (let ( (k (- (string-length num) 1)))
@@ -78,8 +83,9 @@ compito:
     )
   )
 
-(define bit-val
-  (lambda (bit)
+; Se char = 0 : 0 , negli altri casi 1
+(define bit-val   ; val: carattere (0/1)
+  (lambda (bit)   ; bit: carattere
     (if (char=? bit #\0) 0 1)
     )
   )
